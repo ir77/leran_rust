2421cd90e63a1314cb199d0d1b0d7d47ee4c9883
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2020-04-17 23:24:33 +0900

    first commit

---
+++ hello_world/target/debug/deps/hello_world-705f118cccad1752.d
@@ -0,0 +1,5 @@
+/Users/ucucmacmini/Workspace/rustProjects/hello_world/target/debug/deps/hello_world-705f118cccad1752: src/main.rs
+
+/Users/ucucmacmini/Workspace/rustProjects/hello_world/target/debug/deps/hello_world-705f118cccad1752.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/ucucmacmini/Workspace/rustProjects/hello_world/target/debug/hello_world: /Users/ucucmacmini/Workspace/rustProjects/hello_world/src/main.rs
