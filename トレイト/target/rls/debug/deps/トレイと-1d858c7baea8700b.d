1ffbaeb98316fc09ff6c9e0ed2104c617eb865bd
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2020-05-03 18:16:09 +0900

    トレイトの章を追加

---
+++ トレイト/target/rls/debug/deps/トレイと-1d858c7baea8700b.d
@@ -0,0 +1,5 @@
+/Users/ucucmacmini/Workspace/code/rust_introduction/トレイト/target/rls/debug/deps/トレイと-1d858c7baea8700b.rmeta: src/main.rs
+
+/Users/ucucmacmini/Workspace/code/rust_introduction/トレイト/target/rls/debug/deps/トレイと-1d858c7baea8700b.d: src/main.rs
+
+src/main.rs:
---
+++ トレイト/target/rls/debug/deps/トレイと-ca1a6534a6cc707a.d
@@ -0,0 +1,5 @@
+/Users/ucucmacmini/Workspace/code/rust_introduction/トレイト/target/rls/debug/deps/トレイと-ca1a6534a6cc707a.rmeta: src/main.rs
+
+/Users/ucucmacmini/Workspace/code/rust_introduction/トレイト/target/rls/debug/deps/トレイと-ca1a6534a6cc707a.d: src/main.rs
+
+src/main.rs:
