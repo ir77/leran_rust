d159629f78c4d146793e43f325df3e3183dac249
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2020-05-10 16:01:55 +0900

    add

---
+++ city-pop/src/main.rs
@@ -0,0 +1,33 @@
+extern crate getopts;
+extern crate rustc_serialize;
+
+use getopts::Options;
+use std::env;
+
+fn print_usage(program: &str, opts: Options) {
+    println!(
+        "{}",
+        opts.usage(&format!("Usage: {} [options] <data-path> <city>", program))
+    );
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "Show this usage message.");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(e) => panic!(e.to_string()),
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+    let data_path = args[1].clone();
+    let city = args[2].clone();
+
+    // 情報を元にいろいろなことをする
+}
--- エラーハンドリング/src/main.rs
+++ エラーハンドリング/src/main.rs
@@ -1,3 +1,86 @@
+use std::env;
+use std::error::Error;
+use std::fs::File;
+use std::io::Read;
+use std::num::ParseIntError;
+use std::path::Path;
+
+// 1から10までの数字を予想します。
+// もし予想した数字に一致したらtrueを返し、そうでなければfalseを返します。
+fn guess(n: i32) -> bool {
+    if n < 1 || n > 10 {
+        panic!("Invalid number: {}", n);
+    }
+    n == 5
+}
+
+// `haystack`（干し草の山）からUnicode文字 `needle`（縫い針）を検索します。
+// もし見つかったら、文字のバイトオフセットを返します。見つからなければ、`None` を
+// 返します。
+fn find(haystack: &str, needle: char) -> Option<usize> {
+    for (offset, c) in haystack.char_indices() {
+        if c == needle {
+            return Some(offset);
+        }
+    }
+    None
+}
+
+// 与えられたファイル名の拡張子を返す。拡張子の定義は、最初の
+// `.` に続く、全ての文字である。
+// もし `file_name` に `.` がなければ、`None` が返される。
+fn extension(file_name: &str) -> Option<&str> {
+    find(file_name, '.').map(|i| &file_name[i + 1..])
+}
+
+fn double_number(number_str: &str) -> Result<i32, ParseIntError> {
+    match number_str.parse::<i32>() {
+        Ok(n) => Ok(2 * n),
+        Err(err) => Err(err),
+    }
+}
+
+fn double_number2(number_str: &str) -> Result<i32, ParseIntError> {
+    number_str.parse::<i32>().map(|n| 2 * n)
+}
+
+fn double_arg(mut argv: env::Args) -> Result<i32, String> {
+    argv.nth(1)
+        .ok_or("Please give at least one argument".to_owned())
+        .and_then(|arg| arg.parse::<i32>().map_err(|err| err.to_string()))
+        .map(|n| 2 * n)
+}
+
 fn main() {
-    println!("Hello, world!");
+    // #1
+    // guess(11);
+
+    // #2
+    // let mut argv = env::args();
+    // let arg: String = argv.nth(1).unwrap(); // エラー1
+    // let n: i32 = arg.parse().unwrap(); // エラー2
+    // println!("{}", 2 * n);
+
+    // #3
+    let file_name = "foobar.rs";
+    match find(file_name, '.') {
+        None => println!("No file extension found."),
+        Some(i) => println!("File extension: {}", &file_name[i + 1..]),
+    }
+
+    // #4
+    assert_eq!(extension("foobar.csv").unwrap_or("rs"), "csv");
+    assert_eq!(extension("foobar").unwrap_or("rs"), "rs");
+
+    // #5
+    match double_number2("10") {
+        Ok(n) => assert_eq!(n, 20),
+        Err(err) => println!("Error: {:?}", err),
+    }
+
+    // #6
+    match double_arg(env::args()) {
+        Ok(n) => println!("{}", n),
+        Err(err) => println!("Error: {}", err),
+    }
 }
